58f3bd2c04db0304f1aff1170ef1f574a3f03114
coleng2 <129232000+coleng2@users.noreply.github.com>
2023-12-01 12:10:35 +0900

    Change the structure of the entity

--- src/entity/CommonUser.java
+++ src/entity/CommonUser.java
@@ -1,17 +1,22 @@
 package entity;
 
+import entity.banks.Bank;
+
 import java.time.LocalDateTime;
 
-public class CommonUser implements User{
+public class CommonUser implements User {
 
     private final String name;
     private final String password;
     private final LocalDateTime creationTime;
 
-    CommonUser(String name, String password, LocalDateTime creationTime) {
+    private Account userAccount;
+
+    CommonUser(String name, String password, LocalDateTime creationTime, Bank bank) {
         this.name = name;
         this.password = password;
         this.creationTime = creationTime;
+        this.userAccount = new Account(name, bank);
     }
 
     @Override
---
+++ src/entity/banks/RBC.java
@@ -0,0 +1,26 @@
+package entity.banks;
+
+import entity.Account;
+
+import java.util.HashMap;
+
+public class RBC implements Bank {
+    public final String bankName = "RBC";
+    private final double exchangeServiceFee = 0.001;
+    private final HashMap<String, Account> accounts = new HashMap<>();
+
+    @Override
+    public double getExchangeServiceFee() {
+        return exchangeServiceFee;
+    }
+
+    @Override
+    public void addUser(String id, Account newAccount) {
+        accounts.put(id, newAccount);
+    }
+
+    @Override
+    public Account getAccount(String id) {
+        return accounts.get(id);
+    }
+}
