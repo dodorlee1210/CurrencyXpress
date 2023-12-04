50fc160f6674a7e164b1b3bdabeffd8aac521878
jinyeong617 <119543270+jinyeong617@users.noreply.github.com>
2023-12-04 18:15:52 -0500

    I have implemented a use case of searchcurrency.

--- src/use_case/search_exchangerate/SearchInteractor.java
+++ src/use_case/search_exchangerate/SearchInteractor.java
@@ -6,30 +6,39 @@ public class SearchInteractor implements SearchInputBoundary {
 
     final SearchOutputBoundary searchPresenter;
 
+    String exchangeResult;
+
 
     public SearchInteractor(SearchDataAccessInterface dataAccessObject, SearchOutputBoundary searchOutputBoundary) {
         this.dataAccessObject = dataAccessObject;
         this.searchPresenter = searchOutputBoundary;
     }
 
-    public void execute(SearchInputData searchInputData) {
+    public String execute(SearchInputData searchInputData) {
         String date = searchInputData.getDate();
         String baseCurrency = searchInputData.getBaseCurrency();
         String symbols = searchInputData.getSymbols();
 
         if (!dataAccessObject.existsByCode(date)) {
             searchPresenter.prepareFailView(date + ": This date is not a valid date.");
+            exchangeResult = "Error:" + " This date is not a valid date.";
         } else if (!dataAccessObject.existsByCode(baseCurrency)) {
             searchPresenter.prepareFailView(baseCurrency + ": Currency Code is not valid as a base currency.");
+            exchangeResult = "Error:" + " Currency code is not valid as a base currency.";
         } else {
             if (baseCurrency.equals(symbols)){
                 searchPresenter.prepareFailView("Exchange does not happen for same currency codes.");
+                exchangeResult = "Error:" + " Exchange does not happen for same currency codes.";
             } else {
                 String[] currency = dataAccessObject.get(symbols).split(":");
-                searchPresenter.prepareSuccessView(new SearchOutputData(currency[0],currency[1],false));
+                SearchOutputData searchOutputData = new SearchOutputData(currency[0],currency[1],false);
+                searchPresenter.prepareSuccessView(searchOutputData);
+
+                exchangeResult = searchOutputData.get_afterSymbols() + "\n"
+                        + searchOutputData.get_afterCurrency();
             }
 
         }
-
+        return exchangeResult;
     }
 }
