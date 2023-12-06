565f2efbba10d5207b5d425edc841487f2320dbe
Dogyu Lee <123223901+dodorlee1210@users.noreply.github.com>
2023-12-06 08:11:32 -0500

    Revert "IN THIS CODE: login works, search works, convert works..."

--- src/interface_adapter/account/AccountPresenter.java
+++ src/interface_adapter/account/AccountPresenter.java
@@ -10,57 +10,42 @@ import interface_adapter.signup.SignupViewModel;
 import use_case.account.AccountInteractor;
 import use_case.account.AccountOutputBoundary;
 import use_case.account.AccountOutputData;
-import interface_adapter.login.LoginState;
-import interface_adapter.login.LoginViewModel;
-import interface_adapter.signup.SignupState;
-import use_case.account.AccountInteractor;
-import interface_adapter.search_exchangerate.SearchState;
-import interface_adapter.search_exchangerate.SearchViewModel;
+
 public class AccountPresenter implements AccountOutputBoundary {
     private final AccountViewModel accountViewModel;
     private final ConvertViewModel convertViewModel;
-    private LoginViewModel loginViewModel;
-    private  SearchViewModel searchViewModel;
     private ViewManagerModel viewManagerModel;
     private LoginViewModel loginViewModel;
 
     public AccountPresenter(ViewManagerModel viewManagerModel, AccountViewModel accountViewModel,
-               ConvertViewModel convertViewModel, LoginViewModel loginViewModel, SearchViewModel searchViewModel) {
-
+                            ConvertViewModel convertViewModel, LoginViewModel loginViewModel) {
         this.viewManagerModel = viewManagerModel;
         this.accountViewModel = accountViewModel;
         this.convertViewModel = convertViewModel;
         this.loginViewModel = loginViewModel;
-
-        this.searchViewModel = searchViewModel;
     }
 
+    @Override
     public void prepareSuccessView(AccountOutputData response) {
         if (response.getMethod().equals("logout")) {
+            // Reset textfields/input for login page
             LoginState loginState = loginViewModel.getState();
-            loginState.setUsername(response.getUsername());
+            loginState.setUsername("");
+            loginState.setPassword("");
             loginViewModel.setState(loginState);
             loginViewModel.firePropertyChanged();
+
             viewManagerModel.setActiveView(loginViewModel.getViewName());
             viewManagerModel.firePropertyChanged();
         } else if (response.getMethod().equals("exchange")) {
+            // Switch to convert page
             ConvertState convertState = convertViewModel.getState();
             convertState.setUsername(response.getUsername());
             this.convertViewModel.setState(convertState);
             this.convertViewModel.firePropertyChanged();
+
             viewManagerModel.setActiveView(convertViewModel.getViewName());
             viewManagerModel.firePropertyChanged();
-        } else if (response.getMethod().equals("search")) {
-            SearchState searchState = searchViewModel.getState();
-            searchState.setUsername(response.getUsername());
-            this.searchViewModel.setState(searchState);
-            this.searchViewModel.firePropertyChanged();
-            viewManagerModel.setActiveView(searchViewModel.getViewName());
-            viewManagerModel.firePropertyChanged();
         }
-
-
     }
-
-
 }
--- src/interface_adapter/account/AccountState.java
+++ src/interface_adapter/account/AccountState.java
@@ -3,18 +3,21 @@ package interface_adapter.account;
 public class AccountState {
     private String username;
     private String method;
+    private String bank;
+    private String balance;
+    private String[][] currencies;
 
 
     public AccountState(AccountState copy) {
-
         username = copy.username;
         method = copy.method;
-
+        bank = copy.bank;
+        balance = copy.balance;
+        currencies = copy.currencies;
     }
 
     // Because of the previous copy constructor, the default constructor must be explicit.
-    public AccountState() {
-    }
+    public AccountState() {}
 
     public String getUsername() {
         return username;
@@ -24,7 +27,17 @@ public class AccountState {
         return method;
     }
 
+    public String getBank() {
+        return bank;
+    }
 
+    public String getBalance() {
+        return balance;
+    }
+
+    public String[][] getCurrencies() {
+        return currencies;
+    }
 
     public void setUsername(String username) {
         this.username = username;
@@ -34,4 +47,15 @@ public class AccountState {
         this.method = method;
     }
 
+    public void setBank(String bank) {
+        this.bank = bank;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
+
+    public void setCurrencies(String[][] currencies) {
+        this.currencies = currencies;
+    }
 }
