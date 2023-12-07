bba07d96f13347fa315f24f7e74b4b60854da153
coleng2 <129232000+coleng2@users.noreply.github.com>
2023-12-07 19:42:07 +0900

    Add javadoc for the Bank.java (interface)

--- src/entity/banks/Bank.java
+++ src/entity/banks/Bank.java
@@ -1,7 +1,15 @@
 package entity.banks;
 
 public interface Bank {
+    /**
+     * The bank has its own unique exchange service fee (esf) and this method will return the esf.
+     * @return the bank's exchange service fee
+     */
     double getExchangeServiceFee();
 
+    /**
+     * The bank has its own unique name and this method will return the bank's name
+     * @return bank name
+     */
     String getBankName();
 }
