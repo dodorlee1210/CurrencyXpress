6e649c45b73221213af339890a063bac6c34804c
coleng2 <129232000+coleng2@users.noreply.github.com>
2023-12-07 23:17:30 +0900

    Add javadoc for the ExchangeHistory.java

--- src/entity/ExchangeHistory.java
+++ src/entity/ExchangeHistory.java
@@ -3,11 +3,11 @@ package entity;
 import java.time.LocalDateTime;
 
 public class ExchangeHistory {
-    private String sourceCurrency;
-    private String targetCurrency;
-    private double exchangedAmount;
-    private double exchangeRate;
-    private LocalDateTime exchangeTime;
+    private final String sourceCurrency;
+    private final String targetCurrency;
+    private final double exchangedAmount;
+    private final double exchangeRate;
+    private final LocalDateTime exchangeTime;
 
     // Constructor
     public ExchangeHistory(String sourceCurrency, String targetCurrency, double exchangedAmount, double exchangeRate) {
@@ -19,26 +19,44 @@ public class ExchangeHistory {
     }
 
     // Getters
+    /**
+     * @return the currency code that is being exchanged
+     */
     public String getSourceCurrency() {
         return sourceCurrency;
     }
 
+    /**
+     * @return the currency code that is being received from the exchange service
+     */
     public String getTargetCurrency() {
         return targetCurrency;
     }
 
+    /**
+     * @return the amount (result) of exchanged currency
+     */
     public double getExchangedAmount() {
         return exchangedAmount;
     }
 
+    /**
+     * @return the exchange rate (depends on the currency codes, ex: 1 eur = 1.47 cad)
+     */
     public double getExchangeRate() {
         return exchangeRate;
     }
 
+    /**
+     * @return return the date and time of exchanged occurred
+     */
     public LocalDateTime getExchangeTime() {
         return exchangeTime;
     }
-    // Exchange
+
+    /**
+     * @return string representation of this object
+     */
     public String toString() {
         return String.format("Source Currency: %s, Target Currency: %s, Exchanged Amount: %.2f, Exchange Rate: %.4f, Exchange Time: %s",
                 sourceCurrency, targetCurrency, exchangedAmount, exchangeRate, exchangeTime);
