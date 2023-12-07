96248d251ab0d745114614eb62b0852635c1f175
coleng2 <129232000+coleng2@users.noreply.github.com>
2023-12-07 20:02:36 +0900

    Add javadoc

--- src/entity/PasswordChecker.java
+++ src/entity/PasswordChecker.java
@@ -1,6 +1,11 @@
 package entity;
 
 public class PasswordChecker implements PasswordCheck{
+    /**
+     * The method to verify the password validity
+     * @param password password to check the validity
+     * @return true if the password is valid, false otherwise.
+     */
     @Override
     public boolean passwordIsValid(String password) {
         return password != null && password.length() == 8;
