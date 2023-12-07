838cedb6761082f48724363f9d38fb7e17a1e69e
coleng2 <129232000+coleng2@users.noreply.github.com>
2023-12-07 19:25:50 +0900

    Add javadoc for the BMO.java file

--- src/entity/banks/BMO.java
+++ src/entity/banks/BMO.java
@@ -6,11 +6,19 @@ public class BMO implements Bank {
     public final String bankName = "BMO";
     private final double exchangeServiceFee = 0.025;
 
+    /**
+     * The BMO bank's exchange service fee charge (%) represented as double value
+     * @return BMO's exchange service fee
+     */
     @Override
     public double getExchangeServiceFee() {
         return exchangeServiceFee;
     }
 
+    /**
+     * Get the bank name
+     * @return the bank name: BMO
+     */
     @Override
     public String getBankName() {
         return bankName;
