4bac900bebe0557ba3782aeaaca6103ba85358dc
coleng2 <129232000+coleng2@users.noreply.github.com>
2023-12-07 19:22:55 +0900

    Add javadoc for the CommonUserFactory.java file and deleted the unused class

--- src/entity/CommonUserFactory.java
+++ src/entity/CommonUserFactory.java
@@ -4,6 +4,15 @@ import entity.banks.Bank;
 
 public class CommonUserFactory implements UserFactory {
 
+    /**
+     * Create the user object from the given parameters
+     * @param username          user's username to log in
+     * @param password          user's password to log in
+     * @param bank              the bank user is using
+     * @param initialBalance    the initial balance when user is creating the account
+     * @param accountHolder     user's name
+     * @return the user object
+     */
     @Override
     public User create(String username, String password, Bank bank, double initialBalance, String accountHolder) {
         return new CommonUser(username, password, bank, initialBalance, accountHolder);
